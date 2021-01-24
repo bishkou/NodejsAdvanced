f7c52a59ea4800b45121f9181b600014afaf7554
bishkou <chedyhammami@yahoo.com>
2021-01-24 23:20:24 +0100

    caching complete

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const cleanCache = require('../middlewares/cleanCache');
 
 const Blog = mongoose.model('Blog');
 
+
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+  app.get('/api/blogs/:id', requireLogin, async (req, res, next) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
@@ -14,23 +16,16 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis');
-    const redisUrl = 'redis://127.0.0.1:6379';
-    const client = redis.createClient(redisUrl);
-    const util = require('util');
-
-    client.get = util.promisify(client.get);
-
-    const cachedBlogs = await client.get(req.user.id);
-
-
-
-    const blogs = await Blog.find({ _user: req.user.id });
+    const blogs = await Blog.find({ _user: req.user.id })
+        .cache({
+          key: req.user.id
+        });
 
     res.send(blogs);
+
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
