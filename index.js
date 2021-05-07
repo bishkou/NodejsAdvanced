250ce70bb31dc9e1ec2046b3a353de8cdb065852
bishkou <chedyhammami@yahoo.com>
2021-05-07 11:53:45 +0100

    fix

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ require('./services/passport');
 require('./services/cache');
 
 // mongoose.Promise = global.Promise;
-
 mongoose.connect(keys.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
